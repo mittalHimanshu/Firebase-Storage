919f8d97929978e330b04175c95d3ecc58d29b79
mittalHimanshu <mittalhimanshu121@gmail.com>
2018-08-19 22:09:04 +0530

    validations remaining

--- app/src/main/java/com/example/phoenix/firebaseuploader/ImageActivity.java
+++ app/src/main/java/com/example/phoenix/firebaseuploader/ImageActivity.java
@@ -3,7 +3,6 @@ package com.example.phoenix.firebaseuploader;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -29,7 +28,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
