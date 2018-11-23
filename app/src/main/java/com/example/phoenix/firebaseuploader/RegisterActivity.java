dcd1735cbda2de6fa7016394a067db668a0a2fc7
Himanshu Mittal <mittalhimanshu121@gmail.com>
2018-11-23 12:36:18 +0530

    bugs fixed

--- app/src/main/java/com/example/phoenix/firebaseuploader/RegisterActivity.java
+++ app/src/main/java/com/example/phoenix/firebaseuploader/RegisterActivity.java
@@ -1,9 +1,13 @@
 package com.example.phoenix.firebaseuploader;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,6 +29,9 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class RegisterActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
@@ -34,6 +41,7 @@ public class RegisterActivity extends AppCompatActivity {
     static String fileExtension1;
     File file;
     ImageView imageView;
+    public static final int RequestPermissionCode = 7;
     private static final int GALLERY = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,18 +50,63 @@ public class RegisterActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         email1 = findViewById(R.id.email);
         password1 = findViewById(R.id.password);
-        imageView = findViewById(R.id.imageView10);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+
+            case RequestPermissionCode:
+
+                if (grantResults.length > 0) {
+
+                    boolean readStoragePermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStoragePermission = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (readStoragePermission && writeStoragePermission) {
+                        Snackbar.make(findViewById(R.id.constraintLayout), "Thanks for granting Permissions", Snackbar.LENGTH_SHORT).show();
+                    } else {
+                        Snackbar.make(findViewById(R.id.constraintLayout), "Please grant all Permissions !!!", Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+
+                break;
+        }
+    }
+
+    private void RequestMultiplePermission() {
+
+        ActivityCompat.requestPermissions(RegisterActivity.this, new String[]
+                {
+                        READ_EXTERNAL_STORAGE,
+                        WRITE_EXTERNAL_STORAGE
+                }, RequestPermissionCode);
+
+    }
+
+    public boolean CheckingPermissionIsEnabledOrNot() {
+
+        int FirstPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        int SecondPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+
+        return FirstPermissionResult == PackageManager.PERMISSION_GRANTED &&
+                SecondPermissionResult == PackageManager.PERMISSION_GRANTED;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            Toast.makeText(getApplicationContext(), "Already Logged In", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(RegisterActivity.this, ImageActivity.class);
-            startActivity(intent);
-            finish();
+        if (!CheckingPermissionIsEnabledOrNot())
+            RequestMultiplePermission();
+        else {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser != null) {
+                Toast.makeText(getApplicationContext(), "Already Logged In", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(RegisterActivity.this, ImageActivity.class);
+                startActivity(intent);
+                finish();
+            }
         }
     }
 
@@ -86,14 +139,6 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(intent);
         finish();
-    }
-
-    public void selectProfileImage(View view) {
-
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        startActivityForResult(intent, GALLERY);
-
     }
 
     @Override
